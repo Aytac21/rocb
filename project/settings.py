7c6dfe783ce503de56fea7b2e95518b1b0414d8c
NicatDev <nicat254memmedov@gmail.com>
2024-06-10 20:57:45 +0400

    22

--- project/settings.py
+++ project/settings.py
@@ -63,7 +63,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'language.DefaultLanguageMiddleware',
+    # 'language.DefaultLanguageMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
