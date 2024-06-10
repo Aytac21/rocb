80f1739666dd992c281a524ececda727a2b6bf69
NicatDev <nicat254memmedov@gmail.com>
2024-06-10 20:32:55 +0400

    22

--- project/settings.py
+++ project/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'language.DefaultLanguageMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
+    # 'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
