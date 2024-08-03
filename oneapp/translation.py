070ef358715b2b37d15e59f415e5418b8a0d6c4e
NicatDev <nicat254memmedov@gmail.com>
2024-08-03 17:31:11 +0400

    'MetaTagTranslation'

--- oneapp/translation.py
+++ oneapp/translation.py
@@ -4,23 +4,34 @@ from oneapp.models import Blog,BlogSection,Service,ServiceSection,Portfolio,Port
 class MetaTranslationOption(TranslationOptions):
     fields = ('meta_title','meta_description')
 
+translator.register(MetaInfo, MetaTranslationOption)
+
 class BlogTranslationOption(TranslationOptions):
     fields = ('title','description','meta_title','meta_description')
 
+class BlogSectionTranslationOption(TranslationOptions):
+    fields = ('title','description')
+
 translator.register(Blog, BlogTranslationOption)
-translator.register(BlogSection, BlogTranslationOption)
+translator.register(BlogSection, BlogSectionTranslationOption)
 
 class ServiceTranslationOption(TranslationOptions):
     fields = ('title','description','meta_title','meta_description')
 
+class ServiceSectionTranslationOption(TranslationOptions):
+    fields = ('title','description')
+
 translator.register(Service, ServiceTranslationOption)
-translator.register(ServiceSection, ServiceTranslationOption)
+translator.register(ServiceSection, ServiceSectionTranslationOption)
 
 class PortfolioTranslationOption(TranslationOptions):
     fields = ('title','description','meta_title','meta_description')
 
+class PortfolioSectionTranslationOption(TranslationOptions):
+    fields = ('title','description')
+
 translator.register(Portfolio, PortfolioTranslationOption)
-translator.register(PortfolioSection, PortfolioTranslationOption)
+translator.register(PortfolioSection, PortfolioSectionTranslationOption)
 
 class TestimonialTranslationOption(TranslationOptions):
     fields = ('title','description','field')
